5e31b5e644bd954b2a902d1ff7a2f84ec00e1e0b
yan442512318 <yan442512318@gmail.com>
2022-06-05 11:14:05 +0800

    Change the inverse_rank

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ df = pd.read_csv(input_csv, header=None, names=['word', 'word_frequency'])
 
 df['rank'] = df['word_frequency'].rank(ascending=False, method='max')
 
-df['inverser_rank'] = 1/df['rank']
+df['inverser_rank'] = 1/df['rank'] + 0.1
 scatterplot = df.plot.scatter(x='word_frequency',y='inverser_rank', figsize=[12,6],grid=True)
 fig = scatterplot.get_figure()
 fig.savefig('results/jane_eyre.png')
